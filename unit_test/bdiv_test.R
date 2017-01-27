c184b0ae1aba95280ce65aae63f41b3e5e16c00a
Will Pearse <will.pearse@gmail.com>
2017-01-27 10:59:15 -0700

    Checked work

--- unit_test/bdiv_test.R
+++ unit_test/bdiv_test.R
@@ -1,7 +1,16 @@
+#Headers
 library(testthat)
+source("../community1.R")
+
+#Load in data
+test_matrix <- read.csv("test_matrix.csv")
+
+#Perform tests
 expect_that(b.div.wrap(test_matrix), equals(structure(list(total = 2.6, species = 
                                                              structure(c(0.257142857142857, 0.257142857142857, 0.257142857142857, 
-                                                                         0.228571428571429), .Names = c("Species A", "Species B", 
-                                                                                                        "Species C", "Species D")), sites = c(0.204761904761905, 
+                                                                         0.228571428571429), .Names = c("Species.A", "Species.B", 
+                                                                                                        "Species.C", "Species.D")), sites = c(0.204761904761905, 
                                                                                                                                               0.147619047619048, 0.147619047619048, 0.147619047619048, 0.204761904761905, 
                                                                                                                                               0.147619047619048)), .Names = c("total", "species", "sites"))))
+
+#Bodie - write a test that makes sure the three "sides" of the equation tally up
